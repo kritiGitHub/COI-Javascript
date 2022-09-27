96cf185a4e79accd172e77116425e64bb63d12df
Kriti Jain <kriti.jain@jktech.com>
2022-09-27 16:04:36 +0530

    JS - Variable, operator, Data Type
    Variable, operator, Data Type

---
+++ basics-assignment-1-start/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+let userInput = 10;
+let result;
+
+result = userInput + 18;
+
+result = result - 8;
+result = result * 2;
+result = result / 10;
+
+alert('userInput-' + userInput);
+alert('result-' + result);
