7d24db1161028fdbeabfdc1a3b36931b2f7fa673
Kriti Jain <kriti.jain@jktech.com>
2022-09-27 18:20:10 +0530

    JS - Assignment 2

--- assignment-2/assignment.js
+++ assignment-2/assignment.js
@@ -1,3 +1,4 @@
+const task3Element = document.getElementById('task-3');
 
 function ShowRandomText(){
     alert("Hi Default function");
@@ -7,5 +8,12 @@ function ShowTextWithParameter(name){
     alert("Hi function with Parameter - " + name);
 }
 
+function Input3StringsOutputString(name1, name2, name3){
+    alert("Hi function with 3 Parameter - "+ name1 +" " +  name2 + " " + name3);
+}
+
 ShowRandomText();
 ShowTextWithParameter("Kriti");
+Input3StringsOutputString("Javascript", "TypeScript", "Angular");
+
+task3Element.addEventListener("click", ShowRandomText);
